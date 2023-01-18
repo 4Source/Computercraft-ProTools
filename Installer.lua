f8cb7443e2c36552493d7f9e0e539ccbd33e5f84
4Source <38220764+4Source@users.noreply.github.com>
2023-01-18 20:18:59 +0100

    Functional Installer

--- Installer.lua
+++ Installer.lua
@@ -4,7 +4,8 @@
 -- License: MIT 
 -- GitHub: https://github.com/4Source/Computercraft-ProTools
 -- Pastebin: https://pastebin.com/wHmS4pNS
--- Installation: pastebin run wHmS4pNS
+-- Install: 'pastebin run wHmS4pNS'
+-- Update: 'pastebin run wHmS4pNS update'
 
 ----------- Helper functions -----------
 function ensureOverride(program)
@@ -20,7 +21,7 @@ function ensureOverride(program)
 end 
 
 function removeProgram(path)
-    if fs.exist(path) then 
+    if fs.exists(path) then 
         local sure 
         while sure == nil do 
             sure = ensureOverride(path)
@@ -29,20 +30,47 @@ function removeProgram(path)
             fs.delete(path)
             return true
         else 
+            print("Canceled...")
             return false
         end
     else 
-        return false
+        return true
     end
 end
 
 function updateProgram(name, pasteCode)
-    removeProgram(name)
-
-    shell.execute("pastebin", "get "..pasteCode.." "..path)
+	if removeProgram(name) then 
+		shell.run("pastebin", "get", pasteCode, name)
+        print("Update Success.")
+        return
+	end
+    print("Update Failed.")
 end 
+
+function installProgram(name, pasteCode)
+    if fs.exists(name) then 
+        print("Programm "..name.." exist allready. Installion Failed!")
+        print("To update programs use 'update' Argument.")
+        return 
+    end
+    shell.run("pastebin", "get", pasteCode, name)
+    print("Installation Success.")
+end
 
 
 ----------- RUN -----------
--- ExcavatePro Installation
-updateProgram("ExcavatePro", "UmUvXfqs")
+-- Check for input arguments 
+local tArgs = { ... }
+if #tArgs == 1 then 
+    if tArgs[1] == "update" then      
+        -- ExcavatePro Update
+        updateProgram("ExcavatePro", "UmUvXfqs")
+
+    end
+elseif #tArgs == 0 then
+    -- ExcavatePro Installation
+    installProgram("ExcavatePro", "UmUvXfqs")
+
+else 
+    print("Invalid Arguments")
+end
