6bf092c416f20dca20177070abd16beeea5cfa45
4Source <38220764+4Source@users.noreply.github.com>
2023-01-24 22:03:16 +0100

    Installer inproved Output

--- Installer.lua
+++ Installer.lua
@@ -56,6 +56,7 @@ function removeFile(path, force)
 end
 
 function downloadFile(path, pasteCode, force)
+    local success
     if fs.exists(path) then 
         local sure 
         if force then 
@@ -66,18 +67,24 @@ function downloadFile(path, pasteCode, force)
         end
         if sure then 
             fs.delete(path)
-            shell.run("pastebin", "get", pasteCode, path)
+            success = shell.run("pastebin", "get", pasteCode, path)
             print("Update Successful.")
         else 
+            success = false
             print("Update Canceled.") 
         end
     else 
-        shell.run("pastebin", "get", pasteCode, path)
+        success = shell.run("pastebin", "get", pasteCode, path)
         print("Download Successful.")
     end
+    return success
 end 
 
 ----------- RUN -----------
+local install_success = 0
+local install_total = 0
+local remove_success = 0
+local remove_total = 0
 local is_update 
 local clean_state 
 local default_config_excavate_pro 
@@ -89,33 +96,47 @@ if #tArgs == 1 and tArgs[1] == "update" then
     is_update = tArgs[1] == "update"
 elseif #tArgs == 0 then
     is_update = false 
-    default_config_excavate_pro = true
+    --default_config_excavate_pro = true
 else 
     print("Invalid Arguments! Try Again.")
     return 
 end
 
 -- Ask if state should be reset
-while clean_state == nil and not is_update do 
-    clean_state = ensure("Should the state be deleted?")
+while clean_state == nil do 
+    if not fs.exists("/ProTools/state") then 
+        clean_state = false 
+    else
+        clean_state = ensure("Should the state be deleted?")
+    end
 end
 
 -- Ask if config should be reset
-while default_config_excavate_pro == nil and not is_update do 
-    default_config_excavate_pro = ensure("Should the configuration for ExcavatePro be reset to default?")
+while default_config_excavate_pro == nil do 
+    if not fs.exists("/ProTools/ExcavatePro/config") then 
+        default_config_excavate_pro = true 
+    else
+        default_config_excavate_pro = ensure("Should the configuration for ExcavatePro be reset to default?")
+    end
 end
 
 ----- State -----
 -- State Clean Up 
-if is_update or clean_state then 
+if clean_state then 
     -- Delete State File
-    removeFile("/ProTools/state", clean_state)  
+    if removeFile("/ProTools/state", clean_state) then 
+        remove_success = remove_success + 1
+    end
+    remove_total = remove_total + 1
 end 
 
 ----- Config ----- 
 -- ExcavatePro Config Update
-if is_update or default_config_excavate_pro then
-    downloadFile("/ProTools/ExcavatePro/config", "W6KuhCeR", default_config_excavate_pro) 
+if default_config_excavate_pro then
+    if downloadFile("/ProTools/ExcavatePro/config", "W6KuhCeR", default_config_excavate_pro) then 
+        install_success = install_success + 1
+    end 
+    install_total = install_total+ 1
 end
 
 ----- File System -----
@@ -127,23 +148,67 @@ ensurePath("/ProTools/ExcavatePro")
 
 ----- Utilities -----
 -- proUtilities Install/Update 
-downloadFile("/ProTools/Utilities/proUtilities", "nnMQE7U9", is_update)
+if downloadFile("/ProTools/Utilities/proUtilities", "nnMQE7U9", is_update) then 
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- inventoryManager Install/Update
-downloadFile("/ProTools/Utilities/inventoryManager", "9RTF5CDF", is_update)
+if downloadFile("/ProTools/Utilities/inventoryManager", "9RTF5CDF", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- fuelManager Install/Update
-downloadFile("/ProTools/Utilities/fuelManager", "dNEyanjZ", is_update)
+if downloadFile("/ProTools/Utilities/fuelManager", "dNEyanjZ", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- fileManager Install/Update
-downloadFile("/ProTools/Utilities/fileManager", "8nBVnDHu", is_update)
+if downloadFile("/ProTools/Utilities/fileManager", "8nBVnDHu", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- uiUtil Install/Update
-downloadFile("/ProTools/Utilities/uiUtil", "iLvyjQYn", is_update)
+if downloadFile("/ProTools/Utilities/uiUtil", "iLvyjQYn", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- mineUtilities Install/Update
-downloadFile("/ProTools/Utilities/mineUtilities", "gba5abkG", is_update)
+if downloadFile("/ProTools/Utilities/mineUtilities", "gba5abkG", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- stateManager Install/Update
-downloadFile("/ProTools/Utilities/stateManager", "fkVjJJME", is_update)
+if downloadFile("/ProTools/Utilities/stateManager", "fkVjJJME", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 -- configManager Install/Update
-downloadFile("/ProTools/Utilities/configManager", "SkTYkVhV", is_update)
+if downloadFile("/ProTools/Utilities/configManager", "SkTYkVhV", is_update) then
+    install_success = install_success + 1
+end 
+install_total = install_total+ 1
 
 ----- Programs ----- 
 -- ExcavatePro Update
-downloadFile("ExcavatePro", "UmUvXfqs", is_update)
-   
+if downloadFile("ExcavatePro", "UmUvXfqs", is_update) then
+    install_success = install_success + 1
+end 
+install_total= install_total+ 1
+
+----- Finished ----- 
+term.clear()
+term.setCursorPos(1, 1)
+
+if remove_success ~= remove_total then 
+    print("Something went wrong by removing files.")
+elseif install_success ~= install_total then
+    print("Something went wrong by downloading files.")
+else
+    print("Successfully completed.")
+end
+if remove_total > 0 then
+    print("Removed: ("..remove_success.."/"..remove_total..")")
+end
+if install_total > 0 then 
+    print("Downloaded: ("..install_success.."/"..install_total..")")
+end 
