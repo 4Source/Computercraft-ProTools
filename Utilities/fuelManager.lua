ba14d3af8ce1265cc36c6e8e012d17c4e72d9574
4Source <38220764+4Source@users.noreply.github.com>
2023-02-02 20:00:47 +0100

    Added fuelManager

---
+++ Utilities/fuelManager.lua
@@ -0,0 +1,34 @@
+-- ProTools by 4Source 
+-- Fuel Manager 
+-- Version: v0.1.0
+-- License: MIT 
+-- GitHub: https://github.com/4Source/Computercraft-ProTools
+-- Pastebin: https://pastebin.com/dNEyanjZ 
+-- Installation: Run installer below for full installation.
+-- Installer: 'pastebin run wHmS4pNS'
+-- Require: 'local fuel_manager = require("ProTools.Utilities.fuelManager")'
+-- Usage:
+ 
+----------- Formatting -----------
+-- Constants: Uppercase and Underscores (CONSTANTS_EXAMPEL)
+--     boolean: starts with 'b' formulated as a statement (bIS_CONSTANTS_EXAMPEL) 
+-- Variables: Lowercase and Underscores (variable_example)
+--     boolean: formulated as a statement (is_example) 
+-- Functions: Camelcase (functionExample)  
+
+----------- Require -----------
+local log = require("ProTools.Utilities.logger")
+
+----------- Variables -----------
+-- Name of self
+local THIS = "fuel_manager"
+ 
+
+----------- Functions -----------
+
+----------- Run -----------
+
+----------- Return -----------
+return{
+    
+}
